d84c041b9319560ef69deec0d669b3a2751594c9
LeeOrange-is-me <1254216281@qq.com>
2024-08-28 15:40:49 +0800

    lab1

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,27 @@
 #include <string.h>
 #include <unistd.h>
 #include "common.h"
-#include "Ast.h"
-#include "Unit.h"
-#include "MachineCode.h"
-#include "LinearScan.h"
 
 extern FILE *yyin;
 extern FILE *yyout;
-int yyparse();
 
-Ast ast;
-Unit unit;
-MachineUnit mUnit;
+int yylex();
+
 char outfile[256] = "a.out";
 dump_type_t dump_type = ASM;
 
 int main(int argc, char *argv[])
 {
     int opt;
-    while ((opt = getopt(argc, argv, "Siato:")) != -1)
+    while ((opt = getopt(argc, argv, "to:")) != -1)
     {
         switch (opt)
         {
         case 'o':
             strcpy(outfile, optarg);
             break;
-        case 'a':
-            dump_type = AST;
-            break;
         case 't':
             dump_type = TOKENS;
-            break;
-        case 'i':
-            dump_type = IR;
-            break;
-        case 'S':
-            dump_type = ASM;
             break;
         default:
             fprintf(stderr, "Usage: %s [-o outfile] infile\n", argv[0]);
@@ -60,17 +45,6 @@ int main(int argc, char *argv[])
         fprintf(stderr, "%s: fail to open output file\n", outfile);
         exit(EXIT_FAILURE);
     }
-    yyparse();
-    if(dump_type == AST)
-        ast.output();
-    ast.typeCheck();
-    ast.genCode(&unit);
-    if(dump_type == IR)
-        unit.output();
-    unit.genMachineCode(&mUnit);
-    LinearScan linearScan(&mUnit);
-    linearScan.allocateRegisters();
-    if(dump_type == ASM)
-        mUnit.output();
+    yylex();
     return 0;
 }
